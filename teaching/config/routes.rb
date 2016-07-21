2cb7f0f9c2fc51c615e6c6ce1bbcff7000e6d4e4
Jaime <jaivalz@yahoo.com>
2016-07-21 10:57:29 +0000

    Created schools controller, added route

--- teaching/config/routes.rb
+++ teaching/config/routes.rb
@@ -1,10 +1,15 @@
-Rails.application.routes.draw do 
+Rails.application.routes.draw do  
   
   devise_for :users, :controllers => { :sessions => "sessions", :registrations => "registrations" }
+  
+  resources :schools do  
+  end
   
   #Users
   get  "/my_current_user" => "users#my_current_user"
   match 'users/:id' => 'users#update_user', via: [:patch]
   get '/send_password' => "users#reset_password"
+
+  
 
 end
