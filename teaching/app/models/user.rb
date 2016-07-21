2e242eb5c6e16ada55a62f97e922e4bcdc4765d3
Jaime <jaivalz@yahoo.com>
2016-07-21 13:36:08 +0000

    Generated progress model

--- teaching/app/models/user.rb
+++ teaching/app/models/user.rb
@@ -2,8 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :token_authenticatable    
-  
-  has_many :authentication_tokens 
+  :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
+
+  has_many :authentication_tokens
+
+  has_many :progresses
+
+
 end
