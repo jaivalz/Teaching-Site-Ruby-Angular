bbfb31f6d500ec7e1baffae45b0eb4eab8204936
Jaime <jaivalz@yahoo.com>
2016-07-21 12:27:02 +0000

    Created chapter controller and added route as resource

--- teaching/config/routes.rb
+++ teaching/config/routes.rb
@@ -1,16 +1,16 @@
-Rails.application.routes.draw do  
-  
-  devise_for :users, :controllers => { :sessions => "sessions", :registrations => "registrations" }
-  
-  resources :courses
-  
-  resources :schools
-  
-  #Users
-  get  "/my_current_user" => "users#my_current_user"
-  match 'users/:id' => 'users#update_user', via: [:patch]
-  get '/send_password' => "users#reset_password"
+Rails.application.routes.draw do    
 
-  
+  devise_for :users, :controllers => { :sessions => "sessions", :registrations => "registrations" }  
 
-end
+  resources :courses do  
+    resources :chapters  
+  end  
+
+  resources :schools  
+
+  #Users  
+  get  "/my_current_user" => "users#my_current_user"  
+  match 'users/:id' => 'users#update_user', via: [:patch]  
+  get '/send_password' => "users#reset_password"  
+
+end 
